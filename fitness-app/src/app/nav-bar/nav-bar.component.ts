f1befba488631b427857ea8846cd37367a06b990
RileyPike10 <riley.pike10@gmail.com>
2025-04-24 14:57:39 -0500

    messed with the looks of nav bar

--- fitness-app/src/app/nav-bar/nav-bar.component.ts
+++ fitness-app/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterModule],
+  imports: [RouterModule,],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  currentRoute: string = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.capitalizeFirstLetter(this.router.url.split('/')[1]);
+    });
+  }
+
+  capitalizeFirstLetter(route: string): string {
+    if (!route) return '';
+    return route.charAt(0).toUpperCase() + route.slice(1);
+  }
 }
